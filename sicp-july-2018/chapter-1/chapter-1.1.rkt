7beb8473093c200d5d1ce11dadbde3da3bb551b8
Chris Coralli <ccoralli@gmail.com>
2018-07-24 17:36:09 -0400

    indentation

--- sicp-july-2018/chapter-1/chapter-1.1.rkt
+++ sicp-july-2018/chapter-1/chapter-1.1.rkt
@@ -36,11 +36,16 @@
           (- 2
             (- 3
               (+ 6
-                (/ 4 5)))))
+                (/ 4 5)
+              )
+            )
+          )
+      )
       (* 3
         (- 6 2)
-        (- 2 7))
-   )
+        (- 2 7)
+      )
+    )
 
 ; Exercise 1.3:
 ; Define a procedure that takes three numbers as arguments and returns the sum
