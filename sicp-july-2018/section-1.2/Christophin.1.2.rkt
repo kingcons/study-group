da17e60600818680d327bed55f667862390ff458
Chris Coralli <ccoralli@gmail.com>
2018-08-02 14:22:27 -0400

    removed dupe code.

--- sicp-july-2018/section-1.2/Christophin.1.2.rkt
+++ sicp-july-2018/section-1.2/Christophin.1.2.rkt
@@ -64,7 +64,7 @@
                  (A x (- y 1))))))
 
 ; What are the values of the following expressions?
-
+; #1
 (A 1 10)
 (A (- 1 1) (A 1 9))
 (A (- 1 1) (A (- 1 1) (A 1 8)))
@@ -86,6 +86,8 @@
 (A 0 512)
 (1024)
 ; The value is 1024
+
+; #2
 (A 2 4)
 (A (- 2 1) (A 2 3))
 (A (- 2 1) (A (- 2 1) (A 2 2)))
@@ -132,14 +134,9 @@
 (A (- 1 1) (A 0 16384))
 (A 0 32768)
 (65536)
-; Thevalue is 65536
-(define (A x y)
-  (cond ((= y 0) 0)
-        ((= x 0) (* 2 y))
-        ((= y 1) 2)
-        (else (A (- x 1)
-                 (A x (- y 1))))))
+; The value is 65536
 
+; #3
 (A 3 3)
 (A (- 3 1) (A 3 2))
 (A (- 3 1) (A (- 3 1) (A 3 1)))
