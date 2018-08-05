1de003b85a5ff3158a6d6e577030d52980042a2b
Chris Coralli <ccoralli@gmail.com>
2018-08-05 14:35:06 -0400

    finished pascal's triangle recursive function

--- sicp-july-2018/section-1.2/Christophin.1.2.rkt
+++ sicp-july-2018/section-1.2/Christophin.1.2.rkt
@@ -195,24 +195,23 @@
 ; Thevalue is 65536
 
 
+; Exercise 1.12: The following pattern of numbers is called Pascal’s triangle.
 
+;         1
+;       1   1
+;     1   2   1
+;   1   3   3   1
+; 1   4   6   4   1
+;       . . .
+; The numbers at the edge of the triangle are all 1, and each number inside
+; the triangle is the sum of the two numbers above it.35 Write a procedure that
+; computes elements of Pascal’s triangle by means of a recursive process.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+(define (pascal row column) 
+  (cond ((= column 0) 1)
+        ((= column row) 1)
+        (else (+
+                (pascal (- row 1) 
+                  (- column 1))
+                (pascal (- row 1) 
+                  column)))))
