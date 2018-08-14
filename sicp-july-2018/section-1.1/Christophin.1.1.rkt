2fa011af6c5786c6295c6843b49ddf403e59a37f
Chris Coralli <ccoralli@gmail.com>
2018-08-13 21:04:37 -0400

    finished exercise 2.1

--- sicp-july-2018/section-1.1/Christophin.1.1.rkt
+++ sicp-july-2018/section-1.1/Christophin.1.1.rkt
@@ -37,9 +37,9 @@
             (- 3
               (+ 6
                 (/ 4 5)))))
-      (* 3
-        (- 6 2)
-        (- 2 7)))
+        (* 3
+          (- 6 2)
+          (- 2 7)))
 
 ; Exercise 1.3:
 ; Define a procedure that takes three numbers as arguments and returns the sum
---
+++ sicp-july-2018/section-2.1/Christophin.2.1.rkt
@@ -0,0 +1,27 @@
+; Exercise 2.1: Define a better version of make-rat that handles both positive and negative arguments.
+; Make-rat should normalize the sign so that if the rational number is positive, both the numerator
+; and denominator are positive, and if the rational number is negative, only the numerator is negative.
+
+; Original
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (cons (/ n g)
+          (/ d g))))
+
+(define (gcd2 a b)
+  (if (= b 0)
+      a
+      (gcd2 b (remainder a b))))
+
+(define (Make-rat n d)
+    (let ((g ((cond ((and (< n 0) (< d 0)) -)
+                ((and (> n 0) (< d 0)) -)
+               (else +))
+               (gcd n d))))
+        (cons (/ n g)
+        (/ d g))))
+
+(define (Make-rat n d)
+    (let ((g ((if (< d 0) - +) (abs (gcd2 n d)))))
+        (cons (/ n g)
+              (/ d g))))
